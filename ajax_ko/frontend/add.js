aa763271ba18f2ccb98a94489d671a1727ebaa61
maulana ibnu fajar <72007694+maulana421@users.noreply.github.com>
2022-06-27 08:53:08 +0700

    Update add.js

--- ajax_ko/frontend/add.js
+++ ajax_ko/frontend/add.js
@@ -1,37 +1,14 @@
-const btnAdd = document.getElementById("add");
-btnAdd.onclick = function (evt) {
-  const data = {
-    judul_buku: document.getElementById("judul_buku").value,
-    penulis: document.getElementById("penulis").value,
-    penerbit: document.getElementById("penerbit").value,
-  };
-
-  const xhttp = new XMLHttpRequest();
-  xhttp.open(
-    "POST",
-    "http://localhost/ajax_ko/backend/tambah_proses.php",
-    true
-  );
-  xhttp.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-  xhttp.send(JSON.stringify(data));
-  xhttp.onreadystatechange = function () {
-     var result = JSON.parse(this.responseText);
-     if (result.status == "success") {
-       window.location.href = "index.html";
-     } else {
-       alert("Eror");
-     }
-   };
-   xhttp.onerror = function () {
-     alert("Terjadi kesahalan");
-   };
-
-  // xhttp.onload = function () {
-  //     if (xhttp.status >= 200) {
-  //         alert("Successfully add data");
-  //     }
-  // }
-
-  
-  // window.location.href = "index.html";
-}
+$(document).on("click", "#add", function () {
+  var buku = new Object();
+  buku.judul_buku = $("#judul_buku").val();
+  buku.penulis = $("#penulis").val();
+  buku.penerbit = $("#penerbit").val();
+  $.post("../backend/tambah_proses.php", JSON.stringify(buku), function (resp,status) {
+    let result = resp;
+    if (result.status == "success") {
+      window.location.href = "index.html";
+    } else {
+      alert("data gagal tersimpan");
+    }
+  });
+});
